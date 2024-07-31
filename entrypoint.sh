139d0eddc9ac09b60b05181ee9e6a28a81315563
Roman Ivanov <sitdoff@gmail.com>
2024-07-31 14:30:42 +0300

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,15 @@
 # Устанавливаем строгий режим
 set -e
 
-python manage.py collectstatic
-
 # Ожидание доступности базы данных
 ./wait-for-it.sh db:5432 -- echo "PostgreSQL is ready"
 
 # Применение миграций, если это необходимо
-python manage.py makemigrations && python manage.py migrate
+python manage.py makemigrations 
+python manage.py migrate
+
+# Сбор статических файлов
+# python manage.py collectstatic --noinput --clear
 
 # Запуск вашего Django приложения
 exec "$@"
