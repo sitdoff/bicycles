886aeb7449c875f6fff239642d78ed8f685d7e21
Roman Ivanov <sitdoff@gmail.com>
2024-08-01 14:44:07 +0300

    update enrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ python manage.py makemigrations  && python manage.py migrate  && python manage.p
 python manage.py collectstatic --noinput --clear
 
 # Запуск тестов
-pytest --no-warning
+pytest --disable-warnings
 
 # Запуск вашего Django приложения
 exec "$@"
