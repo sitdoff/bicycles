3a9da928b6acc828cccbad2956c14c4f5283a198
Roman Ivanov <sitdoff@gmail.com>
2024-08-01 12:25:37 +0300

    add __init__.py

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,10 @@ python manage.py makemigrations
 python manage.py migrate
 
 # Сбор статических файлов
-# python manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
+
+# Загрузка данных для демонстрации
+python manage.py loaddata demo_fixture.json
 
 # Запуск вашего Django приложения
 exec "$@"
