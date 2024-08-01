93de12f86e18104360f61ed5dd014bb07d458def
Roman Ivanov <sitdoff@gmail.com>
2024-08-01 13:57:31 +0300

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,14 @@
 set -e
 
 # Ожидание доступности базы данных
-./wait-for-it.sh db:5432 -- echo "PostgreSQL is ready"
+./wait-for-it.sh -t 30 db:5432 -- echo "PostgreSQL is ready"
 
 # Применение миграций, если это необходимо
-python manage.py makemigrations 
-python manage.py migrate
+# Загрузка данных для демонстрации
+python manage.py makemigrations  && python manage.py migrate  && python manage.py loaddata demo_fixture.json
 
 # Сбор статических файлов
 python manage.py collectstatic --noinput --clear
-
-# Загрузка данных для демонстрации
-python manage.py loaddata demo_fixture.json
 
 # Запуск вашего Django приложения
 exec "$@"
